6e29b662c250c21b11404447347dfaf2b9bb03f4
halvo78 <220444059+halvo78@users.noreply.github.com>
2025-10-15 07:51:49 -0400

    Add deployment script

---
+++ DEPLOY_NOW.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# AUTO-DEPLOYMENT SCRIPT
+# Run this on your local Ubuntu to deploy the ULTIMATE TURBO-CHARGED SYSTEM
+
+cd ~/ultimate_lyra_systems
+wget https://github.com/halvo78/sandy---box/raw/main/ULTIMATE_TURBO_COMPLETE_PACKAGE.tar.gz
+tar -xzf ULTIMATE_TURBO_COMPLETE_PACKAGE.tar.gz
+chmod +x DEPLOY_ON_LOCAL_UBUNTU.sh
+./DEPLOY_ON_LOCAL_UBUNTU.sh
